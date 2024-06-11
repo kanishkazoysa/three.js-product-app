2010bca37ce4d0b592838dfff5a5e9f2c164202d
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-11 23:18:19 +0530

    setup valtio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import {motion, AnimatePresence} from 'framer-motion';
 import {useSnapshot} from 'valtio';
+import state from '../store';
 import{
   headContainerAnimation,
   headContentAnimation,
-  sideAnimation
+  slideAnimation
 } from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>
-      Home
-    </div>
+  <AnimatePresence>
+  {snap.intro && (
+    <motion.section className='home' {...slideAnimation('left')}>
+     <motion.header>
+     <img
+      src='./threejs.png'
+      alt='logo'
+      className="w-8 h-8 object-contain"
+      />
+     </motion.header>
+    </motion.section>
+  )}
+  </AnimatePresence>
   )
 }
 
