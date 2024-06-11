b93ab45d7e6392825223e1e281681eef81decc95
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-11 23:20:04 +0530

    update animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   <AnimatePresence>
   {snap.intro && (
     <motion.section className='home' {...slideAnimation('left')}>
-     <motion.header>
+     <motion.header {...slideAnimation("down")}>
      <img
       src='./threejs.png'
       alt='logo'
