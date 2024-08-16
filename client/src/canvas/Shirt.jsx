c79fd0ebdb9d03dcaa0cb57938711d22b780e330
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-08-16 19:24:01 +0530

    edit

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -13,7 +13,14 @@ const Shirt = () => {
 
   return (
     <group>
-    
+    <mesh
+    castShadow
+    geometry={nodes.T_Shirt_male.geometry}
+    material={materials.lambert1}
+    material-roughness={1}
+    dispose={null}
+    >
+    </mesh>
     </group>
   )
 }
