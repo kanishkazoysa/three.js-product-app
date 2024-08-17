801f46f4754ae6587bdafb0553ded1c00b7fbdc0
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-08-18 02:51:21 +0530

    last edit

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,11 +6,30 @@ dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY,});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
-router.route
+router.route("/").post(async (req, res) => {
+  try {
+    const { promt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n:1,
+      size:'1024x1024',
+      response_format: 'b64_json'
+
+    });
+
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo:image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "something went wrong"});
+  }
+});
+
+router.route;
 export default router;
