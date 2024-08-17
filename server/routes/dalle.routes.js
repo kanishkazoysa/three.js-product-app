ec322c46c322a714f7a9016361ecac810bc3fd49
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-08-17 15:46:35 +0530

    edit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY,});
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
+
+router.route
+export default router;
