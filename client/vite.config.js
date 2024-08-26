905d66b2db7467511d22c3cd3bbed11a44526707
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-08-26 19:47:48 +0530

    add docker file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // This makes the server accessible externally
+    port: 5173,  // Specify the port
+  },
 })
